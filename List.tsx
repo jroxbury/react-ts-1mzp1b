efcb975d4152c7a8be79e800ca7f7c5a3e0a4b90
jroxbury <roxbury.joseph@gmail.com>
2021-01-23 12:44:44 -0700

    1 file modified

--- List.tsx
+++ List.tsx
@@ -1,36 +1,34 @@
-import React, { useState } from "react";
-
-const initialData = [
-  {
-    name: "Joe",
-    age: 30
-  },
-  {
-    name: "Blair",
-    age: 30
-  },
-  {
-    name: "Ted",
-    age: 30
-  }
-];
+import React, { useState, useEffect } from "react";
+// import { HiOutlineShoppingCart } from "react-icons/hi";
+import ClipLoader from "react-spinners/ClipLoader";
 
 export function List(props) {
-  const [data, setData] = useState(initialData);
+  const [data, setData] = useState<Product[]>([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then(res => res.json())
+      .then(products => setData(products));
+  }, []);
   return (
     <div>
       <ul>
-        {data.map(item => {
-          return (
-            <li>
-              Name: {item.name}, Age: {item.age}
-            </li>
-          );
-        })}
+        {data.length ? (
+          data.map(item => {
+            return <li>{item.title}</li>;
+          })
+        ) : (
+          <ClipLoader />
+        )}
       </ul>
-      <button onClick={() => setData([...data, { name: "Guy", age: 30 }])}>
-        Add
-      </button>
     </div>
   );
+}
+
+interface Product {
+  id: number;
+  title: string;
+  price: string;
+  category: string;
+  description: string;
+  image: string;
 }
